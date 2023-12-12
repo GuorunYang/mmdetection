958c7842b5351661711566c97ed8e945258030c6
Guorun Yang <guorun.yang@autra.tech>
2023-12-12 18:15:18 +0800

    Script: score threshold from 0.7 to 0.8

--- scripts/inference_dino_tag.sh
+++ scripts/inference_dino_tag.sh
@@ -9,8 +9,8 @@ RECORD_PATH=$1
 python3 scripts/grounding_dino_mine.py \
     $RECORD_PATH \
     weights/swinb_autra_train_finetune_epoch_20.pth \
-    --tag-score-thr 0.7 \
-    --no-save-vis \
-    --no-save-pred \
-    --batch-size 64
+    --tag-score-thr 0.8 \
+    --batch-size 64 \
+    # --no-save-vis \
+    # --no-save-pred \
     # --is_dev
