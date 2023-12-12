3e1a79071bb797f120a53f7b3d314dcbfa24d3ce
Guorun Yang <guorun.yang@autra.tech>
2023-12-12 20:58:27 +0800

    Model: update mine model

--- scripts/inference_dino_tag.sh
+++ scripts/inference_dino_tag.sh
@@ -8,9 +8,10 @@ RECORD_PATH=$1
 
 python3 scripts/grounding_dino_mine.py \
     $RECORD_PATH \
-    weights/swinb_autra_train_finetune_epoch_20.pth \
+    weights/swinb_autra_trainval_finetune_epoch_19.pth \
     --tag-score-thr 0.8 \
     --batch-size 64 \
-    --no-save-vis \
-    --no-save-pred \
+    # --is_dev 
+    # --no-save-vis \
+    # --no-save-pred \
     # --is_dev
