a609ccb12cad12378579fcb84e8e23bdbaac90f1
Guorun Yang <guorun.yang@autra.tech>
2023-12-12 18:29:00 +0800

    Refine: no save visualizations

--- scripts/inference_dino_tag.sh
+++ scripts/inference_dino_tag.sh
@@ -11,6 +11,6 @@ python3 scripts/grounding_dino_mine.py \
     weights/swinb_autra_train_finetune_epoch_20.pth \
     --tag-score-thr 0.8 \
     --batch-size 64 \
-    # --no-save-vis \
-    # --no-save-pred \
+    --no-save-vis \
+    --no-save-pred \
     # --is_dev
